07eef4d5875fbb5110309e4326b17046e1ed717a
Dennylson Costa <dennylsonpdr@gmail.com>
2021-01-02 01:21:34 -0300

    Full pipeline tes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Meu app com pipeline na AWS!'));
+app.get('/', (req, res) => res.send('Meu app com pipeline na AWS! Agora com aprovacao manual.));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
